217732faa4d7067cc23fd6d2863009edb766f002
niubin261 <niubin@mail.ustc.edu.cn>
2017-06-08 19:18:39 +0800

    Role Message Fixed --NiuBin

--- src/main/java/org/onosproject/floodlightpof/protocol/OFRoleReply.java
+++ src/main/java/org/onosproject/floodlightpof/protocol/OFRoleReply.java
@@ -24,7 +24,7 @@
  */
 
 package org.onosproject.floodlightpof.protocol;
-
+import org.jboss.netty.buffer.ChannelBuffer;
 import org.onosproject.floodlightpof.util.U16;
 
 /**
@@ -32,10 +32,76 @@ import org.onosproject.floodlightpof.util.U16;
  *
  */
 public class OFRoleReply extends OFMessage {
+    public static int minimumlength = 12;
+    protected OFControllerRole ofControllerRole;
     //TODO implement
     public OFRoleReply() {
         super();
         this.type = OFType.ROLE_REPLY;
-        this.length = U16.t(MINIMUM_LENGTH);
+        this.length = (short)minimumlength;
+        this.ofControllerRole = null;
     }
+    public void readFrom(ChannelBuffer data) {
+        super.readFrom(data);
+        ofControllerRole = OFControllerRole.values()[data.readByte()];
+        data.readBytes(3);
+    }
+    /**
+     * @return the ofControllerRole
+     */
+    public OFControllerRole getOfControllerRole() {
+        return ofControllerRole;
+    }
+    /**
+     * @param ofControllerRole
+     * the ofControllerRole to set
+     */
+    public void setOfControllerRole(OFControllerRole ofControllerRole) {
+        this.ofControllerRole = ofControllerRole;
+    }
+
+    @Override
+    public void writeTo(ChannelBuffer data) {
+        super.writeTo(data);
+        if (ofControllerRole != null) {
+            data.writeByte((byte) ofControllerRole.ordinal());
+            data.writeZero(3);
+        }
+    }
+    @Override
+    public String toString() {
+        String string = super.toString();
+        string += ";OFControllerRole=" + this.ofControllerRole;
+        return string;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result += result*prime + ((ofControllerRole == null) ? 0 : ofControllerRole.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object object) {
+        if (this==object){
+            return true;
+        }
+        if (!super.equals(object)) {
+            return false;
+        }
+        if (!(object instanceof OFRoleRequest)) {
+            return false;
+        }
+        OFRoleRequest other = (OFRoleRequest) object;
+        if (ofControllerRole == null) {
+            if (other.ofControllerRole != null) {
+                return false;
+            }
+        } else if (!ofControllerRole
+                .equals(other.ofControllerRole)) {
+            return false;
+        }
+        return true;
+    }
+
 }
--- src/main/java/org/onosproject/floodlightpof/protocol/OFRoleRequest.java
+++ src/main/java/org/onosproject/floodlightpof/protocol/OFRoleRequest.java
@@ -25,17 +25,90 @@
 
 package org.onosproject.floodlightpof.protocol;
 
+import org.jboss.netty.buffer.ChannelBuffer;
 import org.onosproject.floodlightpof.util.U16;
+
+import java.util.logging.Logger;
 
 /**
  * TODO Not used in POF yet.
  *
  */
 public class OFRoleRequest extends OFMessage {
+
+    public static int minimumLength = 12;
+    protected OFControllerRole ofControllerRole;
+
     //TODO implement
     public OFRoleRequest() {
         super();
         this.type = OFType.ROLE_REQUEST;
-        this.length = U16.t(MINIMUM_LENGTH);
+        this.length=(short)minimumLength;
+        this.ofControllerRole = null;
+    }
+
+    @Override
+    public void readFrom(ChannelBuffer data) {
+        super.readFrom(data);
+        ofControllerRole = OFControllerRole.values()[data.readByte()];
+        data.readBytes(3);
+    }
+
+    /**
+     * @return the ofControllerRole
+     */
+    public OFControllerRole getOfControllerRole() {
+        return ofControllerRole;
+    }
+    /**
+     * @param ofControllerRole
+     * the ofControllerRole to set
+     */
+    public void setOfControllerRole(OFControllerRole ofControllerRole) {
+        this.ofControllerRole = ofControllerRole;
+    }
+
+    @Override
+    public void writeTo(ChannelBuffer data) {
+        super.writeTo(data);
+        if (ofControllerRole != null) {
+            data.writeByte((byte) ofControllerRole.ordinal());
+            data.writeZero(3);
+        }
+    }
+    @Override
+    public String toString() {
+        String string = super.toString();
+        string += ";OFControllerRole=" + this.ofControllerRole;
+        return string;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result += result*prime + ((ofControllerRole == null) ? 0 : ofControllerRole.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object object) {
+        if (this==object){
+            return true;
+        }
+        if (!super.equals(object)) {
+            return false;
+        }
+        if (!(object instanceof OFRoleRequest)) {
+            return false;
+        }
+        OFRoleRequest other = (OFRoleRequest) object;
+        if (ofControllerRole == null) {
+            if (other.ofControllerRole != null) {
+                return false;
+            }
+        } else if (!ofControllerRole
+                .equals(other.ofControllerRole)) {
+            return false;
+        }
+        return true;
     }
 }
